4ba9e9a8506e1acf55e3399ff0c85989379396e4
hhduc <hhduc@tma.com.vn>
2023-08-29 15:49:03 +0700

    Merge Sort

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import algorithm.MergeSort;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] arr = new int[]{1, 9, 4, 3, 2, 6, 5, 7, 8, 2, 1};
+        System.out.println("Before: " + Arrays.toString(arr));
+        MergeSort.sort(arr);
+        System.out.println("After : " + Arrays.toString(arr));
     }
 }
--- src/algorithm/MergeSort.java
+++ src/algorithm/MergeSort.java
@@ -1,2 +1,52 @@
-package algorithm;public class MergeSort {
+package algorithm;
+
+public class MergeSort {
+
+    public static void sort(int[] arr) {
+        if (arr == null || arr.length < 2) return;
+        sort(arr, 0, arr.length - 1);
+    }
+
+    private static void sort(int[] arr, int l, int r) {
+        if (l >= r) return;
+        int m = (l + r) / 2;
+        sort(arr, l, m);
+        sort(arr, m + 1, r);
+        merge(arr, l, m, r);
+    }
+
+    private static void merge(int[] arr, int l, int m, int r) {
+        int i, j, k;
+        int n1 = m - l + 1, n2 = r - m;
+        int[] L = new int[n1], R = new int[n2];
+        for (i = 0; i < n1; i++) {
+            L[i] = arr[l + i];
+        }
+        for(j = 0; j < n2; j++) {
+            R[j] = arr[m + 1 + j];
+        }
+        i = 0;
+        j = 0;
+        k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] < R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
 }
